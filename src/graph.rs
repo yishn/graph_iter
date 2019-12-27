990f06bd580fd9d52a3247a955eddbaf506bdf81
Yichuan Shen <shen.yichuan@gmail.com>
2019-12-27 01:36:26 +0100

    Add 'potentially infinite' to the description

--- src/graph.rs
+++ src/graph.rs
@@ -4,7 +4,7 @@ use edge::{Edge, WeightedEdge};
 use vertex_container::{DfsContainer, BfsContainer};
 use vertex_iterator::{DefaultVertexIter, DijkstraVertexIter};
 
-/// Represents a directed graph.
+/// Represents a directed, potentially infinite, graph.
 pub trait Graph<V: Vertex> {
   /// Generates a list of vertices that can be reached from `vertex` by traveling along an edge.
   fn get_neighbors(&self, vertex: &V) -> Vec<V>;
@@ -24,7 +24,7 @@ pub trait Graph<V: Vertex> {
   }
 }
 
-/// Represents a directed multigraph, where edges contain certain data.
+/// Represents a directed, potentially infinite, multigraph, where edges contain certain data.
 pub trait EdgedGraph<V: Vertex, E: Edge>: Graph<V> {
   /// Generates a list of edges that connect `vertex` with `other`.
   fn get_edges(&self, vertex: &V, other: &V) -> Vec<E>;
