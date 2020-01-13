1d6dd53c2f4d9340213fc6c94769b8bba146bd97
Yichuan Shen <shen.yichuan@gmail.com>
2020-01-13 22:50:42 +0100

    Fix FiniteGraph::remove_vertex()

--- src/finite_graph.rs
+++ src/finite_graph.rs
@@ -58,20 +58,18 @@ impl<V, E> FiniteGraph<V, E> {
   }
 
   pub fn edges(&self) -> impl Iterator<Item = &E> {
-    self.edges_map.values()
-    .map(|(e, _, _)| e)
+    self.edges_map.values().map(|(e, _, _)| e)
   }
 
   pub fn edges_mut(&mut self) -> impl Iterator<Item = &mut E> {
-    self.edges_map.values_mut()
-    .map(|(e, _, _)| e)
+    self.edges_map.values_mut().map(|(e, _, _)| e)
   }
 
-  pub fn get(&self, vertex: Id) -> Option<&V> {
+  pub fn get_vertex(&self, vertex: Id) -> Option<&V> {
     self.vertices_map.get(&vertex)
   }
 
-  pub fn get_mut(&mut self, vertex: Id) -> Option<&mut V> {
+  pub fn get_vertex_mut(&mut self, vertex: Id) -> Option<&mut V> {
     self.vertices_map.get_mut(&vertex)
   }
 
@@ -83,6 +81,14 @@ impl<V, E> FiniteGraph<V, E> {
     self.edges_map.get_mut(&edge).map(|(e, _, _)| e)
   }
 
+  pub fn contains_vertex(&self, vertex: Id) -> bool {
+    self.vertices_map.contains_key(&vertex)
+  }
+
+  pub fn contains_edge(&self, edge: Id) -> bool {
+    self.edges_map.contains_key(&edge)
+  }
+
   pub fn insert_vertex(&mut self, data: V) -> Id {
     let id = self.id.next();
     self.vertices_map.insert(id, data);
@@ -92,54 +98,53 @@ impl<V, E> FiniteGraph<V, E> {
 
   pub fn remove_vertex(&mut self, vertex: Id) -> Option<V> {
     let result = self.vertices_map.remove(&vertex);
-    let neighbors = self.neighbors_map.remove(&vertex).unwrap_or_else(|| vec![]);
+    self.neighbors_map.remove(&vertex);
 
-    for (neighbor, _) in neighbors {
-      if let Some(neighbors) = self.neighbors_map.get_mut(&neighbor) {
-        let index = neighbors.iter().position(|(x, _)| x == &vertex);
+    let to_delete = self.edges_map.iter()
+      .filter(|(_, (_, v, w))| v == &vertex || w == &vertex)
+      .map(|(edge, _)| *edge)
+      .collect::<Vec<_>>();
 
-        if let Some(index) = index {
-          neighbors.remove(index);
-        }
-      }
+    for edge in to_delete {
+      self.remove_edge(edge);
     }
 
     result
   }
 
-  fn insert_edge_id(&mut self, start: Id, end: Id, edge: Id) -> Option<Id> {
-    if let Some(neighbors) = self.neighbors_map.get_mut(&start) {
-      neighbors.push((end, edge));
+  fn insert_edge_id(&mut self, from: Id, to: Id, edge: Id) -> Option<Id> {
+    if let Some(neighbors) = self.neighbors_map.get_mut(&from) {
+      neighbors.push((to, edge));
     } else {
-      self.neighbors_map.insert(start, vec![(end, edge)]);
+      self.neighbors_map.insert(from, vec![(to, edge)]);
     }
 
     Some(edge)
   }
 
-  pub fn insert_edge(&mut self, start: Id, end: Id, data: E) -> Option<Id> {
-    if !self.vertices_map.contains_key(&start) || !self.vertices_map.contains_key(&end) {
+  pub fn insert_edge(&mut self, from: Id, to: Id, data: E) -> Option<Id> {
+    if !self.vertices_map.contains_key(&from) || !self.vertices_map.contains_key(&to) {
       return None;
     }
 
     let id = self.id.next();
-    self.edges_map.insert(id, (data, start, end));
+    self.edges_map.insert(id, (data, from, to));
 
-    self.insert_edge_id(start, end, id)
+    self.insert_edge_id(from, to, id)
   }
 
-  pub fn insert_bi_edge(&mut self, vertex: Id, other: Id, data: E) -> Option<Id> {
-    let edge = self.insert_edge(vertex, other, data);
+  pub fn insert_bi_edge(&mut self, from: Id, to: Id, data: E) -> Option<Id> {
+    let edge = self.insert_edge(from, to, data);
 
     if let &Some(id) = &edge {
-      self.insert_edge_id(other, vertex, id);
+      self.insert_edge_id(to, from, id);
     }
 
     edge
   }
 
   pub fn remove_edge(&mut self, edge: Id) -> Option<E> {
-    self.edges_map.remove(&edge).map(|(result, vertex, other)| {
+    self.edges_map.remove(&edge).map(|(data, vertex, other)| {
       for vertex in &[vertex, other] {
         self.neighbors_map.get_mut(&vertex).map(|neighbors| {
           neighbors.iter().position(|(_, e)| e == &edge).map(|index| {
@@ -148,7 +153,7 @@ impl<V, E> FiniteGraph<V, E> {
         });
       }
 
-      result
+      data
     })
   }
 }
@@ -189,24 +194,54 @@ mod tests {
   type Position = (i32, i32);
 
   #[test]
-  fn insert_vertices_and_edges() {
-    let mut graph = FiniteGraph::<Position, ()>::new();
+  fn insert_and_remove_vertices_and_edges() {
+    let mut graph = FiniteGraph::<Position, usize>::new();
 
     let a = graph.insert_vertex((0, 0));
     let b = graph.insert_vertex((0, 1));
     let c = graph.insert_vertex((1, 1));
     let d = graph.insert_vertex((1, 0));
 
-    graph.insert_edge(a, b, ());
-    graph.insert_edge(a, c, ());
-    graph.insert_edge(a, d, ());
-    graph.insert_edge(b, c, ());
-    graph.insert_edge(d, c, ());
+    let e1 = graph.insert_edge(a, b, 1).unwrap();
+    let e2 = graph.insert_edge(a, c, 2).unwrap();
+    let e3 = graph.insert_edge(a, d, 3).unwrap();
+    let e4 = graph.insert_edge(b, c, 4).unwrap();
+    let e5 = graph.insert_edge(d, c, 5).unwrap();
 
     assert_eq!(graph.vertices().count(), 4);
-    assert_eq!(graph.neighbors(&a).len(), 3);
-    assert_eq!(graph.neighbors(&b).len(), 1);
-    assert_eq!(graph.neighbors(&c).len(), 0);
-    assert_eq!(graph.neighbors(&d).len(), 1);
+    assert_eq!(graph.edges().count(), 5);
+
+    assert_eq!(graph.get_vertex(a).unwrap(), &(0, 0));
+    assert_eq!(graph.get_vertex(b).unwrap(), &(0, 1));
+    assert_eq!(graph.get_vertex(c).unwrap(), &(1, 1));
+    assert_eq!(graph.get_vertex(d).unwrap(), &(1, 0));
+
+    assert_eq!(graph.get_edge(e1), Some(&1));
+    assert_eq!(graph.get_edge(e2), Some(&2));
+    assert_eq!(graph.get_edge(e3), Some(&3));
+    assert_eq!(graph.get_edge(e4), Some(&4));
+    assert_eq!(graph.get_edge(e5), Some(&5));
+
+    assert_eq!(graph.neighbors(&a), vec![b, c, d]);
+    assert_eq!(graph.neighbors(&b), vec![c]);
+    assert_eq!(graph.neighbors(&c), vec![]);
+    assert_eq!(graph.neighbors(&d), vec![c]);
+
+    let edge_data = graph.remove_edge(e3);
+
+    assert_eq!(edge_data, Some(3));
+    assert_eq!(graph.get_edge(e3), None);
+    assert_eq!(graph.vertices().count(), 4);
+    assert_eq!(graph.edges().count(), 4);
+    assert_eq!(graph.neighbors(&a), vec![b, c]);
+    assert_eq!(graph.neighbors(&d), vec![c]);
+
+    let vertex_data = graph.remove_vertex(b);
+
+    assert_eq!(vertex_data, Some((0, 1)));
+    assert_eq!(graph.get_vertex(b), None);
+    assert_eq!(graph.vertices().count(), 3);
+    assert_eq!(graph.edges().count(), 2);
+    assert_eq!(graph.neighbors(&a), vec![c]);
   }
 }
